c0acafc588382deed8c273097aa68f8581fcbe81
NaotoF <2016asap@gmail.com>
2022-09-12 23:27:32 +0900

    [WIP]ベストアンサー機能を実装

--- app/models/best_answer.rb
+++ app/models/best_answer.rb
@@ -2,8 +2,4 @@ class BestAnswer < ApplicationRecord
   belongs_to :student
   belongs_to :teacher
   belongs_to :question
-
-  def best_answer_selected?(question)
-    exists?(question_id: question.id)
-  end
 end
