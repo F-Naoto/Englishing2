f5301a89c3f0419c2d2550e931d7e659c0961818
NaotoF <2016asap@gmail.com>
2022-10-03 17:08:22 +0900

    [WIP]herokuエラーの解消

--- config/initializers/csso.rb
+++ config/initializers/csso.rb
@@ -11,8 +11,7 @@ class CssoCompressor
     temp_file.flush
 
     # Run the compressor and capture the output
-    css, err, status = Open3.capture3("npx", "csso", temp_file.path)
-    {data: css}
+
   end
 end
 
